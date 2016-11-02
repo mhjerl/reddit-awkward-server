a04375272c853b1f929801d0fec3832d5ed6b85f
Morten Hjerl-Hansen <mortenhh@gmail.com>
2016-11-03 00:24:58 +0100

    authenticate with redditor/hash

--- authenticate.php
+++ authenticate.php
@@ -11,13 +11,20 @@ if (!isset($_GET['hash'])) {
 	exit();
 }
 
+if (!isset($_GET['redditor'])) { 
+	$data['msg'] = "noredditor";
+	echo json_encode($data);
+	exit();
+}
+
+
 $data = Array();
 
 
+$redditor = $_GET['redditor'];
 $hash = $_GET['hash'];
 
-
-$query4 = "SELECT redditor FROM prima_user WHERE hash='$hash';";
+$query4 = "SELECT * FROM prima_user WHERE redditor='$redditor' AND hash='$hash';";
 $result4 = mysqli_query($GLOBALS["___mysqli_ston"], $query4);
 $num_rows2 = mysqli_num_rows($result4);
 if ($num_rows2 === 0) {
@@ -26,7 +33,6 @@ if ($num_rows2 === 0) {
 	exit();
 }
 else {
-	
 	$data['msg'] = "correcthash";
 	echo json_encode($data);
 	exit();
